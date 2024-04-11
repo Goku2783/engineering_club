aeadae4e1ad17a7ddc7a62acfe6a5710e63074ae
Goku2783 <164094123+Goku2783@users.noreply.github.com>
2024-04-11 15:03:39 +0000

    uodate

---
+++ script.js
@@ -0,0 +1,12 @@
+//opens first semester meeting dates table on click of button.
+function toggleTable1() {
+  document.getElementById("myTable1").classList.toggle("hidden");
+}
+//opens second semester meeting dates table on click of button.
+function toggleTable2() {
+  document.getElementById("myTable2").classList.toggle("hidden");
+}
+
+function toggleDiagram() {
+  document.getElementById("Diagram").classList.toggle("hidden");
+}
