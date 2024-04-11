5a92dae6e72f541cef15e0164eecdf493007c320
Goku2783 <164094123+Goku2783@users.noreply.github.com>
2024-04-11 16:50:10 +0000

    function added for form

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ function toggleTable2() {
 function toggleDiagram() {
   document.getElementById("Diagram").classList.toggle("hidden");
 }
+
+function selectedProjects1() {
+  var projects_group1 = [];
+  var club_one = document.forms("order1").value
+  project_group1.push(club_one);
+  console.log(project_group1);
+}
+
+function selecedProjects2() {
+  var projects_group2 = [];
+  var club_two = document.forms("order2").value
+  project_group2.push(club_two);
+  console.log(project_group2);
+}
+
+//function getID() {
+//   var studentID = [];
+//   var tstStudentIdVal = document.forms["getFormInfo"]["txtStudentID"].value
+//   studentID.push(tstStudentIdVal);
+//   var numberOnly = /^[0-9]+$/;
