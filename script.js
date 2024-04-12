ec87fd251312fdc6595ba2313acff64626103877
Goku2783 <164094123+Goku2783@users.noreply.github.com>
2024-04-12 05:08:51 +0000

    fix

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ function toggleTable1() {
 function toggleTable2() {
   document.getElementById("myTable2").classList.toggle("hidden");
 }
-
+// Opens and hides the EDP diagram on click of button.
 function toggleDiagram() {
   document.getElementById("Diagram").classList.toggle("hidden");
 }
 //prints data from the form into the console by saving it in an array.
+i=0
 function pagePrint1(form1){
   var printdata = document.getElementById("form1").elements;
   var group1 = [];
@@ -21,8 +22,8 @@ function pagePrint1(form1){
 //prints data from the form into the console by saving it in an array.
 function pagePrint2(form2){
   var printdata = document.getElementById("form2").elements;
-  var group1 = [];
+  var group2 = [];
   for (var i = 0; i < printdata.length; i++)
-    group1=group1+printdata[i].value+"\n"
-  console.log(group1);
+    group1=group2+printdata[i].value+"\n"
+  console.log(group2);
 }
