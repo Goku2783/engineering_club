104d7c8b0fd2809e70c91957f83869a0980741ba
Goku2783 <164094123+Goku2783@users.noreply.github.com>
2024-04-30 16:29:04 +0000

    completed website

--- script.js
+++ script.js
@@ -24,66 +24,165 @@ function toggleTable2() {
 function toggleDiagram() {
   document.getElementById("Diagram").classList.toggle("hidden");
 }
-//form1 submitted items
+
+//form1 submitted and saved items
 function Submit1() {
-  var name1 = document.getElementById("name1").value;
-  var parachute1 = document.getElementById("parachute1").value;
+  var name = document.getElementById("name1").value;
+  var parachute = document.getElementById("parachute1").value;
   var Dam = document.getElementById("Dam").value;
   var spider_web = document.getElementById("spider_web").value;
   var errosion_barrier = document.getElementById("errosion_barrier").value;
   var formName = document.getElementById("formName").value; 
-  var comments1 = document.getElementById("comments1").value;
+  var comments = document.getElementById("comments1").value;
   
   var formData = {
     timestamp: Date.now(),
-    name: name1,
-    parachute: parachute1,
+    name: name,
+    parachute: parachute,
     Dam: Dam,
     spider_web: spider_web,
     errosion_barrier: errosion_barrier,
     formName: formName,
-    comments: comments1,
+    comments: comments,
   };
+  //prints data to the console
   console.log("formData: " + formData);
-  return formData;
-}
-
-//this method saves form1 to the database
-function saveForm1() {
-  console.log("saveForm1() called");
-  var formData = Submit1();
+  //save form1 to database
   db.collection("2nd and 3rd Grade").doc(formData.formName).set(formData);
   alert(formData.formName + " save to database!");
-  }
+}
 
-//form2 submitted items
+//This method lets you recall form1 to edit or make changes to it. 
+function editForm1() {
+  console.log("editForm1() called");
+  var formName = prompt("Enter the name of the story you want to edit:");
+  db.collection("2nd and 3rd Grade")
+  .doc(formName)
+  .get()
+  .then((doc) => {
+    if (doc.exists) {
+      console.log("Document data:", doc.data());
+      var formData = doc.data();
+
+      document.getElementById("name1").value = formData.name;
+      document.getElementById("parachute1").value = formData.parachute;
+      document.getElementById("Dam").value = formData.Dam;
+      document.getElementById("spider_web").value = formData.spider_web;
+      document.getElementById("errosion_barrier").value = formData.errosion_barrier;
+      document.getElementById("formName").value = formData.formName;
+      document.getElementById("comments1").value = formData.comments;
+
+    } else {
+      console.log("No such document!");
+      alert(formName + " not found!");
+    }
+  })
+  .catch((error) => {
+    console.log("Error getting document:", error);
+    alert(formName + " not found!");
+  });
+}
+
+//This method deletes the selected form from form1 in the database.
+function deleteForm1() {
+  console.log("deleteForm1() called");
+  var formName = prompt("Enter the name of the story you want to delete:");
+  db.collection("2nd and 3rd Grade")
+  .doc(formName)
+  .get()
+  .then((doc) => {
+    if (doc.exists) {
+      console.log("Document data:", doc.data());
+      var formData = doc.data();
+      document.getElementById("formName").innerHTML = formData.formName + " successfully deleted!";
+      db.collection("2nd and 3rd Grade").doc(formName).delete();
+    } else {
+      console.log("No such document!");
+      alert(formName + " not found!");
+    }
+  })
+  .catch((error) => {
+    console.log("Error getting document:", error);
+    alert(formName + " not found!");  });
+}
+
+//form2 submitted and saved items
 function Submit2() {  
   var name = document.getElementById("name2").value;
-  var parachute2 = document.getElementById("parachute2").value;
+  var parachute = document.getElementById("parachute2").value;
   var magnetic_swing = document.getElementById("magnetic_swing").value;
   var water_filter = document.getElementById("water_filter").value;
   var rocket = document.getElementById("rocket").value;
   var formName = document.getElementById("formName").value;
   var comments = document.getElementById("comments2").value;
   
-  var formData = {timestamp: Date.now(),
-                  name: name,
-                  parachute: parachute2,
-                  magnetic_swing: magnetic_swing,
-                  water_filter: water_filter,
-                  rocket: rocket,
-                  formName: formName,
-                  comments: comments,
-                 };
+  var formData = {
+    timestamp: Date.now(),
+    name: name,
+    parachute: parachute,
+    magnetic_swing: magnetic_swing,
+    water_filter: water_filter,
+    rocket: rocket,
+    formName: formName,
+    comments: comments,
+  };
   //prints data to the console
   console.log("formData: " + formData);
-  return formData;
-}
-
-//this method saves form2 to the database
-function saveForm2() {
-  console.log("saveForm2() called");
-  var formData = Submit2();
+  //Save form2 to database.
   db.collection("4th and 5th Grade").doc(formData.formName).set(formData);
   alert(formData.formName + " save to database!");
+}
+
+//This method lets you recall form2 to edit or make changes to it. 
+function editForm2() {
+  console.log("editForm2() called");
+  var formName = prompt("Enter the name of the story you want to edit:");
+  db.collection("4th and 5th Grade")
+  .doc(formName)
+  .get()
+  .then((doc) => {
+    if (doc.exists) {
+      console.log("Document data:", doc.data());
+      var formData = doc.data();
+
+      document.getElementById("name2").value = formData.name;
+      document.getElementById("parachute2").value = formData.parachute;
+      document.getElementById("magnetic_swing").value = formData.magnetic_swing;
+      document.getElementById("water_filter").value = formData.water_filter;
+      document.getElementById("rocket").value = formData.rocket;
+      document.getElementById("formName").value = formData.formName;
+      document.getElementById("comments2").value = formData.comments;
+
+    } else {
+      console.log("No such document!");
+      alert(formName + " not found!");
+    }
+  })
+  .catch((error) => {
+    console.log("Error getting document:", error);
+    alert(formName + " not found!");
+  });
+}
+
+//This method deletes the selected form from form2 in the database.
+function deleteForm2() {
+  console.log("deleteForm2() called");
+  var formName = prompt("Enter the name of the story you want to delete:");
+  db.collection("4th and 5th Grade")
+  .doc(formName)
+  .get()
+  .then((doc) => {
+    if (doc.exists) {
+      console.log("Document data:", doc.data());
+      var formData = doc.data();
+      document.getElementById("formName").innerHTML = formData.formName + " successfully deleted!";
+      db.collection("4th and 5th Grade").doc(formName).delete();
+    } else {
+      console.log("No such document!");
+      alert(formName + " not found!");
+    }
+  })
+  .catch((error) => {
+    console.log("Error getting document:", error);
+    alert(formName + " not found!");  });
 }
